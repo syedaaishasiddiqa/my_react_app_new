56286cf50aead30924d9c6413408ab18a026a083
syeda <syedaaisha7786@gmail.com>
2023-10-22 11:30:42 -0500

    my_react_app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import { Amplify } from 'aws-amplify';
+import { withAuthenticator } from '@aws-amplify/ui-react';
+import '@aws-amplify/ui-react/styles.css';
+import AnotherComponent from './AnotherComponent';
+import awsExports from './aws-exports';
+import './custom.css';
+import { Link, Route, Routes } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+Amplify.configure(awsExports);
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+function App({ signOut, user }) {
+  return (   
+    
+    <div className="app-header">
+      <h1 className="app-title text-center text-primary">Moxie IT Solutions </h1>
+     <h2 className="heading"> About us</h2><p className="app-paragraph">MoxieIT is a global IT Consulting, Cloud Solutions and Outsourcing firm headquartered in Virginia,USA. 
+
+We go to great lengths to find, attract, hire, motivate, manage, and retain America’s best software engineers and we offer them on demand to our clients at an affordable price and we handle all of the management, administration, infrastructure, development and training.
+
+We consult, architect, build, secure and automate Cloud Solutions. Well versed in the design and implementation of Cloud-Native applications, we embrace the DevOps culture and expertly apply the full AWS feature set.</p>
+      <Link to="/another-component"><button className="btn btn-secondary">Register with us</button></Link>
+
+      <button className="btn btn-primary sign-out-button" onClick={signOut}>Sign out</button>
+    <Routes><Route path="/another-component" element={<AnotherComponent />} />
+    </Routes>
+      </div>
+      
+
+     
   );
 }
 
-export default App;
+export default withAuthenticator(App);
+
