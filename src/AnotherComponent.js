6bd7a9129bc027bb9203528e78c9fb567c91ac51
syeda <syedaaisha7786@gmail.com>
2023-10-22 14:33:39 -0500

    react

---
+++ src/AnotherComponent.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function AnotherComponent() {
+  return (
+    <div>
+      <h2>This is Another Component</h2>
+      <p>Welcome by MoxieIT</p>
+    </div>
+  );
+}
+
+export default AnotherComponent;
